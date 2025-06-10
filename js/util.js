2e62b5fff8fdf1ecfa74bf381e8abbfa42d2a514
NovaBam123 <ibam.nva@gmail.com>
2025-06-11 00:18:34 +0700

    del comment01

--- js/util.js
+++ js/util.js
@@ -2,29 +2,20 @@
 bookmarkedUser.addEventListener('click', e=> {
     const isEdit= e.target.classList.contains('bi-pencil');
     const divContent= e.target.closest('.div-content');
-    
-    // let titleUrl=  e.target.classList.contains('bookmark-name');
+    // const titleUrl=  e.target.classList.contains('bookmark-name');
     if(!divContent) return;
     if(isEdit || e.target.textContent.trim()=== 'Edit'){
         handleEdit(divContent);
     }
-    
-    // if(divContent){
-    //     activeId= id;
- 
-    //     if(isEdit || e.target.textContent.trim()=== 'Edit'){
-           
-    //     }
-    // }
 })
 
 const handleEdit= (element)=> {
-        let id = element.dataset.id;``
+        let id = element.dataset.id;
         let nameUrl= element.dataset.name;
         let color= element.dataset.color;
         let url= element.dataset.url;
         const modalEdit= new bootstrap.Modal(modalPopup);
-        
+
         modalEdit.show();
         activeId = id;
         modalTitle.textContent= 'Edit For a New URL';
